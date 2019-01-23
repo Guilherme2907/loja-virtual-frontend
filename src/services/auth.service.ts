dc618f51372780d3ecbefb361cff4ee415f4a8c2
Guilherme Magalhaes <guilherme.magalhaes2907@gmail.com>
2019-01-23 21:30:43 -0200

    Armazenando email de usuário no localstorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,12 +4,15 @@ import { CredenciaisDTO } from "../models/credenciais.dto";
 import { API_CONFIG } from "../config/api.config";
 import { StorageService } from "./storage.service";
 import { LocalUser } from "../models/local_user";
+import { JwtHelper } from "angular2-jwt";
 
 
 
 
 @Injectable()
 export class AuthService{
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient,public storage: StorageService){
 
@@ -27,7 +30,8 @@ export class AuthService{
     successfulAuth(token: string){
         let tok = token.substring(7);
         let user: LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         }
         this.storage.setLocalUser(user);
     }
