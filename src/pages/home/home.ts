0b9351c008305630cc08fd286054580c73c42fa9
Guilherme Magalhaes <guilherme.magalhaes2907@gmail.com>
2019-01-23 11:46:38 -0200

    Desabilitando menu na tala inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,11 +8,20 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public menuCtrl: MenuController) {
 
+  }
+
+  ionViewWillEnter(){
+    this.menuCtrl.swipeEnable(false);
+  }
+
+  ionViewDidLeave(){
+    this.menuCtrl.swipeEnable(true)
   }
 
   login(){
     this.navCtrl.setRoot('CategoriasPage');
   }
+
 }
