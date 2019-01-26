994cc374d11124a3fe132d763fbd7c70359e73c4
Guilherme Magalhaes <guilherme.magalhaes2907@gmail.com>
2019-01-26 13:26:27 -0200

    Interceptor para adicionar header de autorização nas requisições

---
+++ src/interceptors/interceptor-auth.ts
@@ -0,0 +1,32 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
+import { API_CONFIG } from "../config/api.config";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService){
+
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+
+        let user = this.storage.getLocalUser();
+        let N = API_CONFIG.baseUrl.length;
+        let requestToAPI = req.url.substring(0,N) == API_CONFIG.baseUrl;
+
+        if(user && requestToAPI){
+            const authReq = req.clone({headers: req.headers.set('Authorization' , 'Bearer ' + user.token)});
+            return next.handle(authReq);
+        }
+            return next.handle(req);
+    }
+}
+
+export const AuthInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+}
