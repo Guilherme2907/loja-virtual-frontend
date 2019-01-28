2e963e9ec5f7f8580c04ec6a590d9394e950ec66
Guilherme Magalhaes <guilherme.magalhaes2907@gmail.com>
2019-01-28 13:24:48 -0200

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartItem } from '../../models/cart-item';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 
 @IonicPage()
@@ -19,5 +20,25 @@ export class CartPage {
   ionViewDidLoad() {
     let cart = this.cartService.getCart();
     this.items = cart.items;
+  }
+
+  removeProduto(produto: ProdutoDTO){
+    this.items =  this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items =  this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items =  this.cartService.decreaseQuantity(produto).items;
+  }
+
+  valorTotal(): number{
+    return this.cartService.valorTotal();
+  }
+
+  continue(){
+    this.navCtrl.setRoot("CategoriasPage");
   }
 }
--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -33,5 +33,45 @@ export class CartService {
         this.storage.setCart(cart);
         return cart;
     }
-    
+
+    removeProduto(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(item => item.produto.id == produto.id);
+        if(position != -1){
+            cart.items.splice(position,1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    increaseQuantity(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(item => item.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    decreaseQuantity(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(item => item.produto.id == produto.id);
+        if(position != -1){
+            if(cart.items[position].quantidade > 1){
+                cart.items[position].quantidade--;
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    valorTotal(): number{
+        let cart = this.getCart();
+        let sum = 0;
+        for(var i = 0; i < cart.items.length; i++){
+            sum += cart.items[i].produto.preco * cart.items[i].quantidade;
+        }
+        return sum;
+    }
 }
